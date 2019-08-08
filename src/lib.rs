3e24be077283399d9c6ac4e0c152eb343046fec5
Daniel Lubarov <daniel@lubarov.com>
2019-08-08 10:44:57 -0700

    Add bimap dependency and a helper method for building them
    This is in preparation for permutation networks, coming next.

--- src/lib.rs
+++ src/lib.rs
@@ -10,10 +10,13 @@ pub mod gadget_builder_arithmetic;
 pub mod gadget_builder_binary_arithmetic;
 pub mod gadget_builder_bitwise;
 pub mod gadget_builder_compare;
+pub mod gadget_builder_permutation;
 pub mod gadget_builder_split;
 pub mod expression;
 pub mod wire;
 pub mod witness_generator;
+
+mod bimap_util;
 
 #[cfg(test)]
 mod test_util;
