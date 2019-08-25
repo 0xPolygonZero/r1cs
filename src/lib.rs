14731a4c0e536199284b79d12f3339b48b66ef36
Daniel Lubarov <daniel@lubarov.com>
2019-08-25 13:54:50 -0700

    Make all modules private
    And re-export the public stuff from the root module. I think this will make for a cleaner, and more stable, public API. If the library becomes huge we can revisit this, but for now a flat hierarchy seems best to me.

--- src/lib.rs
+++ src/lib.rs
@@ -6,31 +6,44 @@
 //! See the [readme](https://github.com/mir-protocol/r1cs) for more information and examples.
 
 #[macro_use]
-pub mod wire_values;
+mod wire_values;
 
+mod bimap_util;
 mod binary_arithmetic;
 mod bitwise_operations;
 mod boolean_algebra;
 mod comparisons;
-pub mod constraint;
-pub mod davies_meyer;
-pub mod expression;
-pub mod field;
+mod constraint;
+mod davies_meyer;
+mod expression;
+mod field;
 mod field_arithmetic;
-pub mod gadget;
-pub mod gadget_builder;
-pub mod gadget_traits;
-pub mod merkle_damgard;
-pub mod merkle_trees;
-pub mod mimc;
+mod gadget;
+mod gadget_builder;
+mod gadget_traits;
+mod merkle_damgard;
+mod merkle_trees;
+mod mimc;
 mod permutations;
 mod random_access;
 mod sorting;
 mod splitting;
-pub mod wire;
-pub mod witness_generator;
+mod wire;
+mod witness_generator;
 
-mod bimap_util;
+pub use constraint::*;
+pub use davies_meyer::*;
+pub use expression::*;
+pub use field::*;
+pub use gadget::*;
+pub use gadget_builder::*;
+pub use gadget_traits::*;
+pub use merkle_damgard::*;
+pub use merkle_trees::*;
+pub use mimc::*;
+pub use wire::*;
+pub use witness_generator::*;
+pub use wire_values::*;
 
 #[cfg(test)]
 mod test_util;
