4950e719214a8fe6f78a677d62482f75816628de
Daniel Lubarov <daniel@lubarov.com>
2019-10-05 09:56:13 -0700

    More efficient random access

--- src/random_access.rs
+++ src/random_access.rs
@@ -1,7 +1,9 @@
 //! This module extends GadgetBuilder with methods for randomly accessing lists.
 
-use itertools::enumerate;
+#[cfg(not(feature = "std"))]
+use alloc::vec::Vec;
 
+use crate::BooleanExpression;
 use crate::expression::Expression;
 use crate::field::Field;
 use crate::gadget_builder::GadgetBuilder;
@@ -14,14 +16,56 @@ impl<F: Field> GadgetBuilder<F> {
     /// out-of-range indices, you can do so with a separate call to `assert_lt`.
     // TODO: We can do this more efficiently with a binary MUX structure.
     pub fn random_access(
-        &mut self, items: &[Expression<F>], index: &Expression<F>,
+        &mut self,
+        items: &[Expression<F>],
+        index: &Expression<F>,
     ) -> Expression<F> {
-        let mut result = Expression::zero();
-        for (i, item) in enumerate(items) {
-            let selected = self.equal(index, &Expression::from(i));
-            result += self.product(selected.expression(), item);
+        // Determine the minimum number of bits needed to encode the index.
+        let mut bits = 0;
+        while 1 << bits < items.len() {
+            bits += 1;
         }
-        result
+
+        let index_binary = self.split_bounded(index, bits);
+        self.random_access_binary(items, index_binary.bits)
+    }
+
+    /// Like `random_access`, but with a binary index.
+    fn random_access_binary(
+        &mut self,
+        items: &[Expression<F>],
+        mut index_bits: Vec<BooleanExpression<F>>,
+    ) -> Expression<F> {
+        // Imagine a perfect binary tree whose leaves consist of the given items, followed by zeros
+        // for padding. We can think of each bit of the index as filtering a single layer of the
+        // tree. For example, the first (least significant) index bit selects between pairs of
+        // leaves. After filtering each layer in this manner, we are left with a single value
+        // corresponding to the root of the tree.
+
+        // This leads to a natural recursive solution. Each call of this method will filter the
+        // deepest layer of the tree, then recurse, until we are left with a singleton list.
+
+        if items.len() == 1 {
+            assert!(index_bits.is_empty());
+            return items[0].clone();
+        }
+
+        let lsb = index_bits.remove(0);
+        let num_parents = (items.len() + 1) / 2;
+        let mut parent_layer = Vec::with_capacity(num_parents);
+        for parent_index in 0..num_parents {
+            let left_child_index = parent_index * 2;
+            let right_child_index = parent_index * 2 + 1;
+            let left_child = &items[left_child_index];
+            if right_child_index == items.len() {
+                parent_layer.push(left_child.clone());
+            } else {
+                let right_child = &items[right_child_index];
+                parent_layer.push(self.selection(&lsb, right_child, left_child));
+            }
+        }
+
+        self.random_access_binary(&parent_layer, index_bits)
     }
 }
 
